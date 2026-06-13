66d20cf6447b607d757dfefd4fe3fdab1c7407f2
akvarium11 <slavasal228@gmail.com>
2026-06-13 23:11:33 +0500

    Added time converter :3

--- main.cpp
+++ main.cpp
@@ -46,6 +46,33 @@ int main() {
         }
         logger(1, time_str);
         logger(1, text_str);
+        //
+        size_t colon_pos = time_str.find(':');
+    size_t dot_pos = time_str.find('.');
+
+    if (colon_pos != std::string::npos && dot_pos != std::string::npos) {
+        
+        std::string min_str = time_str.substr(0, colon_pos);
+        std::string sec_str = time_str.substr(colon_pos + 1, dot_pos - colon_pos - 1);
+        std::string ms_str  = time_str.substr(dot_pos + 1);
+
+        int minutes = std::stoi(min_str);
+        int seconds = std::stoi(sec_str);
+        int ms_raw  = std::stoi(ms_str);
+
+        if (ms_str.length() == 2) {
+            ms_raw *= 10;
+        } 
+        else if (ms_str.length() == 1) {
+            ms_raw *= 100;
+        }
+
+        long long total_ms = (minutes * 60000LL) + (seconds * 1000LL) + ms_raw;
+
+        logger(1, to_string(total_ms));
+    } else {
+        logger(0, "Wrong time format!");
+    }
         cout << "=======\n";
       } else {
         logger(0, "Wrong line format!");
